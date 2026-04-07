7ed6df3cd7dfef039dcaa688763a4e06a9d35b37
Marco Santos <marco@sociap.com.br>
2026-04-07 12:51:06 -0300

    ;

--- src/components/ConfigNetwork.tsx
+++ src/components/ConfigNetwork.tsx
@@ -1,5 +1,5 @@
 
-export const CONTRACTID = "14860735cfb7f08f7a790f7a5e8e867f06087d577fb7511bdb654dd625fd5245"
+export const CONTRACTID = "14860735cfb7f08f7a790f7a5e8e867f06087d577fb7511bdb654dd625fd5245";
 // export const CONTRACTID = ""
 // export const CONTRACTID = "927b02ceb1bc3776e87cd5c316b1e43c2a93d6c5b92295f2609c010d1f51a678";
 export const networkId = 'preprod';
