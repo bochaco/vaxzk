fd6d2f2cc3b558a3082fe1113291f96558003af0
curtindoavida <202490+curtindoavida@users.noreply.github.com>
2026-04-08 18:18:31 -0300

    new contract

--- src/components/ConfigNetwork.tsx
+++ src/components/ConfigNetwork.tsx
@@ -1,4 +1,4 @@
-export const CONTRACTID = "8307d1bbc1d95d7c241af41656273d9ee72e696a9e0ddf63c8e266be39e1d0c4";
-// export const CONTRACTID = "";
+// export const CONTRACTID = "8307d1bbc1d95d7c241af41656273d9ee72e696a9e0ddf63c8e266be39e1d0c4";
+export const CONTRACTID = "";
 //export const CONTRACTID = "44b38048104f39006e4aa524644f891f7e3b9278e704ed0a538901b6ffe65f22";
 export const networkId = "preprod";
