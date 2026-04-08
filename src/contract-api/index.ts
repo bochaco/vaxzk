68ace9c532f7a5144f7674d1d9b3af2d37390262
sociapbr <marco@sociap.com.br>
2026-04-08 00:48:26 -0300

    myId and admin.member is different

--- src/contract-api/index.ts
+++ src/contract-api/index.ts
@@ -107,10 +107,10 @@ export class VaxZkAPI implements DeployedVaxZkAPI {
 
         console.log(isAdmin);
         console.log("myId");
-        console.log(toHex(myId));
+        console.log(myId);
         console.log("member");
         for (const admin of ledgerState.admins) {
-          console.log(toHex(admin));
+          console.log(admin);
         }
 
         return { admins, clinics, isAdmin, isClinic };
