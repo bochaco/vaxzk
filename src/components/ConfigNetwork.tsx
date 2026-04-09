37d4162be04f3d850507345d9a45dd86da01c6a7
sociapbr <marco@sociap.com.br>
2026-04-09 17:36:16 -0300

    new contract: 99fd3bb2f32bc5dbc645a3ba8f8ad83d9293c636335abf306adad50d9076b5cd

--- src/components/ConfigNetwork.tsx
+++ src/components/ConfigNetwork.tsx
@@ -1,4 +1,4 @@
-export const CONTRACTID = "";
+export const CONTRACTID = "99fd3bb2f32bc5dbc645a3ba8f8ad83d9293c636335abf306adad50d9076b5cd";
 // export const CONTRACTID = "";
 // export const CONTRACTID = "44b38048104f39006e4aa524644f891f7e3b9278e704ed0a538901b6ffe65f22";
 export const networkId = "preprod";
