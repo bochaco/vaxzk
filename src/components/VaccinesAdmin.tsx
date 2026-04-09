26e30dc61b72c0722194bc892169e29a773bf626
curtindoavida <202490+curtindoavida@users.noreply.github.com>
2026-04-09 11:18:51 -0300

    delete icon

--- src/components/VaccinesAdmin.tsx
+++ src/components/VaccinesAdmin.tsx
@@ -164,11 +164,7 @@ const VaccinesAdmin: React.FC<VaccinesAdminProps> = ({ connectedApi }) => {
                 <span className="font-bold text-lg text-on-surface">{v}</span>
 
                 <a href="#" data-name={v} onClick={handleRemoveVaccine}>
-                  {loading ? (
-                    <><span className="material-symbols-outlined animate-spin">trash</span></>
-                  ) : (
-                    <><span className="material-symbols-outlined">trash</span></>
-                  )}
+                  <span className="material-symbols-outlined animate-spin">delete</span>
                 </a>
               </div>
             ))}
