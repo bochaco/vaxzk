cdb596ab0160183f231376a9c2b0118a34b8e0a1
sociapbr <marco@sociap.com.br>
2026-04-09 06:37:23 -0300

    i18n

--- src/components/VaccinesAdmin.tsx
+++ src/components/VaccinesAdmin.tsx
@@ -72,7 +72,7 @@ const VaccinesAdmin: React.FC<VaccinesAdminProps> = ({ connectedApi }) => {
 
       <section className="mb-12 text-left">
         <h2 className="text-4xl md:text-5xl font-extrabold text-on-surface tracking-tighter mb-4 max-w-2xl">
-          <span className="text-primary">{t.vaccinesAdminTitle}</span> {t.vaccinesAdminTitleEnd} </h2>
+          <span className="text-primary">{t.manage}</span> {t.vaccinesAdminTitleEnd} </h2>
         <p className="text-on-surface-variant text-lg max-w-xl leading-relaxed">{t.vaccinesAdminSubtitle}</p>
       </section>
 
