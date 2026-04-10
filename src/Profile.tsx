50454c8b501716b949d59ab8b5b9d9bc99fd8213
curtindoavida <202490+curtindoavida@users.noreply.github.com>
2026-04-10 16:02:39 -0300

    profile

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,5 +1,5 @@
-import { createContext, useContext, useState } from 'react';
-import Select from 'react-select'
+import { createContext, useContext } from 'react';
+import Select from 'react-select';
 
 const options = [
   { value: 'amdin', label: 'Amdin' },
