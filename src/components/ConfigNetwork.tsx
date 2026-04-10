795af0a60262965d7a8fef2bb4412069117f5206
Gabriel Viganotti <gabrielviganotti@gmail.com>
2026-04-10 19:42:31 -0300

    chore: refactor schnorrVerifyVaxZk to reuse schnorrChallengeVaxZk, add compiled artifacts, update contract address

--- src/components/ConfigNetwork.tsx
+++ src/components/ConfigNetwork.tsx
@@ -1,5 +1,4 @@
-//export const CONTRACTID = "99fd3bb2f32bc5dbc645a3ba8f8ad83d9293c636335abf306adad50d9076b5cd";
 export const CONTRACTID =
-  "6ba6cb5e1b4d03fdce52de72749aa18a8b37673cb9b214d0f8d711d2a94d3f7e";
-// export const CONTRACTID = "44b38048104f39006e4aa524644f891f7e3b9278e704ed0a538901b6ffe65f22";
+  "a891af79f8ad22d64a7137a9877f6f06027b5b9038dcbd1b9b7a41c7f0d40016";
+// export const CONTRACTID = "";
 export const networkId = "preprod";
