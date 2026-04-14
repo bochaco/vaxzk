1a20949a260e82d9266095f10d406ce7541cd9ea
Marco Santos <marco@sociap.com.br>
2026-04-14 18:03:25 -0300

    deploy

--- src/components/DeployContractView.tsx
+++ src/components/DeployContractView.tsx
@@ -55,9 +55,11 @@ const DeployContractView: React.FC<DeployContractProps> = ({onLogout, walletAddr
       if (err && typeof err === 'object' && 'cause' in err) {
         const cause = (err as any).cause;
         var errorMessage = cause?.failure?.message ? String(cause?.failure?.message) : String("");
- //       const txData = cause?.failure?.cause?.txData as Uint8Array;
- //       const hex = toHex(txData);
- //       console.log('txData:', hex);
+        const txData = cause?.failure?.cause?.txData as Uint8Array;
+        const bytes = Object.values(txData);
+        const str = String.fromCharCode(...bytes);
+        console.log(str);
+        errorMessage = errorMessage + '<br>' + str;
         setError(errorMessage);
       }
       setIsDeploying(false);
