461c6781fccd35c42420453c5b356c3c7633aef7
sociapbr <marco@sociap.com.br>
2026-04-14 22:50:59 -0300

    a72243a59217273ff9f0041e23081e05c8482ebdc1a95a01853e6796f7dedb95

--- src/components/ConfigNetwork.tsx
+++ src/components/ConfigNetwork.tsx
@@ -2,6 +2,6 @@
 export const networkId = "preprod";
 
 export function getContractId(): string {
-  return "";
+  return "a72243a59217273ff9f0041e23081e05c8482ebdc1a95a01853e6796f7dedb95";
 //  return "8b8d1f0db613d7762f0398c695ce8f70ea6acd88397a0c9548c89070b6ead397";
 }
