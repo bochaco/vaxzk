5a0fb6baf55fd7cca063f418e1f2bab99a41865e
sociapbr <marco@sociap.com.br>
2026-04-15 10:31:15 -0300

    novo contrato

--- src/components/ConfigNetwork.tsx
+++ src/components/ConfigNetwork.tsx
@@ -3,6 +3,6 @@ export const networkId = "preprod";
 export const urlApp = "https://vaxzk.beerhouse.io/";
 
 export function getContractId(): string {
-  return "";
+  return "b12638166dcebc852bb2035f476b21230d5f20ed6b6936f5429adfb4d70cb2c1";
 //  return "8b8d1f0db613d7762f0398c695ce8f70ea6acd88397a0c9548c89070b6ead397";
 }
