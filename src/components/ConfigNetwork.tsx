8fc9dcf0bf28cc4a806f63e36b95a711557ecc02
Marco Santos <marco@sociap.com.br>
2026-04-20 14:17:39 -0300

    b95c43e20573b28fac9c463d0344f1c31862ef4455d88b3bb2f6c18462e710b6

--- src/components/ConfigNetwork.tsx
+++ src/components/ConfigNetwork.tsx
@@ -3,5 +3,5 @@ export const networkId = "preprod";
 export const urlApp = "https://vaxzk.beerhouse.io";
 
 export function getContractId(): string {
-  return "";
+  return "b95c43e20573b28fac9c463d0344f1c31862ef4455d88b3bb2f6c18462e710b6";
 }
