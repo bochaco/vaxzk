dc07657c019d67b03749b91496077826e1217ca5
Marco Santos <marco@sociap.com.br>
2026-04-23 14:06:43 -0300

    change urlApp to preprod.vaxzk.com.br

--- src/components/ConfigNetwork.tsx
+++ src/components/ConfigNetwork.tsx
@@ -1,5 +1,5 @@
 export const networkId = "preprod";
-export const urlApp = "https://vaxzk.beerhouse.io";
+export const urlApp = "https://preprod.vaxzk.com.br";
 
 export function getContractId(): string {
   return "fa243d504a74514eba82453fc1aae2227b34c7cfab71d6b63de862bc8c19a057";
